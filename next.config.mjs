08e118251f2a5b8d507c2485df0f21ef9ecb9f8a
elengeo <elengeo@umich.edu>
2025-05-11 15:04:05 -0400

    website deployment check

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    assetPrefix: "./",
+  };
+  
+  export default nextConfig;
