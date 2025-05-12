5b3b9512b60f398f0ee5078ba9627b7eadccc251
elengeo <elengeo@umich.edu>
2025-05-12 02:38:50 -0400

    is it updated or what

---
+++ src/app/experiences/page.js
@@ -0,0 +1,14 @@
+// src/app/experiences/page.js
+export default function ExperiencesPage() {
+    return (
+      <div className="text-center">
+        <h1 className="text-4xl mb-4">My Experiences</h1>
+        <ul>
+          <li>Code Instructor at theCoderSchool</li>
+          <li>Student Researcher - Infrastructure as Code Benchmark</li>
+          <li>Web Developer - Instagram Clone</li>
+          <li>Business Sustainability Researcher</li>
+        </ul>
+      </div>
+    );
+  }
